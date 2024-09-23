909a5622243f13ae6485f94be4f5da9975530867
Victor Sepulveda <victorseva123@gmail.com>
2024-09-23 17:53:41 -0500

    actualizacion

--- Pagina Web/src/main.jsx
+++ Pagina Web/src/main.jsx
@@ -18,8 +18,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
