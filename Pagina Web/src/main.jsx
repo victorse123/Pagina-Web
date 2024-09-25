1b559bd3a6dc16bd1c08c95a78faeabd18add922
Victor Sepulveda <victorseva123@gmail.com>
2024-09-25 12:43:38 -0500

    actualizacion

--- Pagina Web/src/main.jsx
+++ Pagina Web/src/main.jsx
@@ -19,15 +19,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
